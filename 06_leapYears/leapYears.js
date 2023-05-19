85d33f8a8465eef76a5d676a7a08b4755f718f48
YoursTrulyZain <61889970+YoursTrulyZain@users.noreply.github.com>
2023-05-19 19:28:21 -0400

    Solve exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    
+    //(Years divisible by 4 and not divisible by 100) or (Years divisible by 100 and divisible by 400)
+    if(((year % 4 == 0) && !(year % 100 == 0) ) || ((year % 100 == 0) && (year % 400 == 0))){
 
+        return true;
+
+    }else{
+
+        return false;
+
+    }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
