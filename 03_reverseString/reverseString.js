cb8cd7c0507684aafe13986513ca2dbc2bb74851
YoursTrulyZain <61889970+YoursTrulyZain@users.noreply.github.com>
2023-05-19 15:06:00 -0400

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let tempString = "";
+
+    for(let i = string.length-1; i >= 0; i--) {
+        tempString += string.charAt(i);
+    }
+
+    return tempString
+    
 };
 
 // Do not edit below this line
