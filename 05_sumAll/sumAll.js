53bfaa6175d807262a7ccab070eee39bffe2a3b6
YoursTrulyZain <61889970+YoursTrulyZain@users.noreply.github.com>
2023-05-19 17:18:21 -0400

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,36 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
 
+    if(((typeof number1) != "number") || ((typeof number2) != "number") || (number1 < 0) || (number2 < 0)){
+
+        return "ERROR";
+
+    }else{
+
+        let totalSum = 0;
+        let i;
+        let j;
+
+        if(number1 < number2){
+
+            i = number1;
+            j = number2;
+
+        }else {
+
+            i = number2;
+            j = number1;
+
+        }
+
+        for(i; i <= j; i++) {
+
+            totalSum += i;
+
+        }
+
+        return totalSum;
+
+    }
 };
 
 // Do not edit below this line
