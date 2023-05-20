939d1d6a31982ae635f04ea6b37012a960db3fc8
YoursTrulyZain <61889970+YoursTrulyZain@users.noreply.github.com>
2023-05-19 20:52:53 -0400

    Solve exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    let cleanRegex = /[\W]/g;
+    let tempString = string;
+    let cleanString = tempString.replaceAll(cleanRegex, "");
+    let reverseString = "";
+
+    for(let i = cleanString.length-1; i >= 0; i--) {
+        reverseString += cleanString.charAt(i);
+    }
+
+    if(reverseString.toLowerCase() === cleanString.toLowerCase()){
+        return true;
+    }else{
+        return false;
+    }
 
 };
+
+palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
