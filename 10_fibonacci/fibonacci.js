113d2631d2ccd967e23e268c4d515801a690f9e3
YoursTrulyZain <61889970+YoursTrulyZain@users.noreply.github.com>
2023-05-19 21:30:33 -0400

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+    if(Number(number) < 0){
+
+        return "OOPS";
+
+    }else{
+
+        let var1 = 0;
+        let var2 = 1;
+        let var3 = 1;
+
+        for(let i = 1; i < Number(number); i++){
+
+            var3 = var1 + var2;
+            var1 = var2;
+            var2 = var3;
+
+        }
+
+        return var3;
+
+    }
 
 };
 
